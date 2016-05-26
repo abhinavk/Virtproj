d7bbf13bd1d6d846bafdc68de012d7efdedf9e20
Abhinav Kumar <abhinav@outlook.com>
2016-05-26 07:12:50 +0530

    Added error page condition

--- ReportGenerator/reportgen.py
+++ ReportGenerator/reportgen.py
@@ -1,9 +1,17 @@
-#!/usr/bin/python3
++#!/usr/bin/python3
 
 import fpdf
 import argparse
 import libvirt
+import time
 
+
+def get_filename(error=False):
+    current_date = 'weekly-report' + time.strftime('%Y-%m-%d')
+    if error is True:
+        return current_date + '-error.pdf'
+    else:
+        return current_date + '.pdf'
 
 def gen_weekly():
     pdf = fpdf.FPDF()
@@ -18,7 +26,22 @@ def gen_weekly():
     print_host_info()
 
     # Write the PDF file
-    pdf.output('weekly-report-2016-05-26.pdf','F')
+    pdf.output(get_filename(),'F')
+
+def gen_error_pdf():
+    errorpdf = fpdf.FPDF()
+    errorpdf.add_page()
+    errorpdf.set_font('Arial','B',48)
+    errorpdf.cell(40, 40, 'Error connecting to Qemu. Terminating report generation.', 0, 0, 'C')
+    pdf.output(get_filename(error=True),'F')
 
 if __name__ == '__main__':
+    # Make connection to libvirt
+    conn = libvirt.open("qemu:///system")
+
+    if conn is None:
+        print("No Qemu instance running.", file=sys.stderr)
+        gen_error_pdf()
+        exit(1)
+
     gen_weekly()
